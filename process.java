2e0289e0893cd5854bc2461d1fcaaf95e4242a53
ahhyun1 <cah3012205@gmail.com>
2024-05-15 17:33:56 +0900

    Create process.java

---
+++ process.java
@@ -0,0 +1,15 @@
+package scheduler;
+
+class process {
+	public int PID;
+	public int arrivalTime;
+	public int burstTime;
+	public int priority;
+	
+	public process(int PID, int arrivalTime, int burstTime, int priority){
+		this.PID = PID;
+		this.arrivalTime = arrivalTime;
+		this.burstTime = burstTime;
+		this.priority = priority;
+	}
+}
