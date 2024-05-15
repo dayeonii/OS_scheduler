c8b3eb3771c3f5a7be21c989fabdfe104ed74354
dayeonii <135932646+dayeonii@users.noreply.github.com>
2024-05-15 13:57:13 +0900

    Delete scheduler_java/os_scheduler/src/process directory

--- scheduler_java/os_scheduler/src/process/process.java
+++
@@ -1,8 +0,0 @@
-package process;
-
-/*  프로세스의 정보를 담은 클래스 (PCB와 비슷한 역할)
-        PID, arrival time(도착시간), Burst time(실행시간), priority(우선순위)
-        ! Timeslice는 어디에 넣지..? 선점형 스케줄러의 클래스에 각자 넣기?
- */
-public class process {
-}
